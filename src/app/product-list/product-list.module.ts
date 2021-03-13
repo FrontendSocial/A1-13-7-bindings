1968d9f53dab2b991bf7fe8cb08d1c1abd4e4f06
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-03-13 21:46:22 +0800

    Two way bining uses and demonstration.

--- src/app/product-list/product-list.module.ts
+++ src/app/product-list/product-list.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SearchFiltersComponent } from './search-filters/search-filters.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [SearchFiltersComponent, ProductListComponent, ProductCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [SearchFiltersComponent, ProductListComponent]
 })
--- src/app/product-list/search-filters/search-filters.component.ts
+++ src/app/product-list/search-filters/search-filters.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-filters.component.scss']
 })
 export class SearchFiltersComponent implements OnInit {
+  searchText = '';
 
   constructor() { }
 
